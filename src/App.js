66511681dc790dc900b701019c83a7d198436b13
rai-n <neeraj.rai@city.ac.uk>
2021-08-26 11:38:04 +0100

    Revert "final"
    This reverts commit 253a70f61b238e8131e6cff0f6de0b4694aa2ea9.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import LocalIssues from './Components/Issues/GetIssue';
-import { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
+<<<<<<< HEAD
 class App extends Component {
   render() {
     return (
@@ -18,8 +16,27 @@ class App extends Component {
       </div>
     );
   }
+=======
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+>>>>>>> parent of fd152b6 (dashboard)
 }
- 
- 
 
 export default App;
