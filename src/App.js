fd152b6072f39a723c8625c87bef9f3fd6511510
rai-n <neeraj.rai@city.ac.uk>
2021-08-26 02:25:43 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LocalIssues from "./Components/Issues/GetIssue";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/styles/default.min.css"
+        />
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+        <LocalIssues></LocalIssues>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Issues/GetIssue.js
@@ -0,0 +1,151 @@
+import issuesJSON from "../../issues.json";
+import React, { Component } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import "../../issue.css";
+class LocalIssues extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      issues: issuesJSON,
+    };
+  }
+
+  render() {
+    const { issues } = this.state;
+
+    let companies = new Set();
+    issuesJSON.forEach((user) => {
+      companies.add(user.Customer);
+    });
+
+    
+    let success = new Set();
+    issuesJSON.forEach((user) => {
+      success.add(user["Success Criteria"]);
+    });
+
+ 
+    return (
+      <div>
+        <h1 className="center">6 month issues exported from YouTrack</h1>
+        <aside>
+          <p>
+            <a href="https://www.w3.org/TR/WCAG21/">
+              Web Content Accessibility Guideline 2.1 AA
+            </a>
+          </p>
+        </aside>
+
+        <Container>
+          <Row>
+            <Col>
+              {" "}
+              <h2>Worked for the following clients:</h2>
+              <ol>
+                {
+                  //converting set to array
+                  [...companies].map((name) => (
+                    <li align="start">
+                      <div>{name}</div>
+                    </li>
+                  ))
+                }
+              </ol>
+            </Col>
+            <Col>
+              {" "}
+              <h2>Reporting issues for the following success criterias</h2>
+              <ul>
+                {
+                  //converting set to array
+                  [...success].map((name) => (
+                    // not using list style 
+                    <li align="start" style={{listStyleType: "none"}}>
+                      <div>{name}</div>
+                    </li>
+                  ))
+                }
+              </ul>
+            </Col>
+            <Col>
+              {" "}
+              <h2>Summary</h2>
+              <p>
+                In this YouTrack export of last six months, I have written well over <b>{issues.length}</b> issues. Identifying {issues.length} that may impact accessibility and usability for users with vision, auditory, motor or cognitive impairments.
+              </p>
+              <p>
+                See the issues below for more detail.
+              </p>
+            </Col>
+          </Row>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </Container>
+      
+        {issues.map((issue) => (
+          <div>
+            <Card style={{ width: "18rem" }} className="container">
+              <ListGroup variant="flush">
+                <ListGroup.Item className="component">
+                  Summary: {issue.Summary}
+                </ListGroup.Item>
+                <ListGroup.Item className="component">
+                  Component: {issue.Component}
+                </ListGroup.Item>
+                <ListGroup.Item className="prior">
+                  Success Criteria: {issue["Success Criteria"]}
+                </ListGroup.Item>
+                <ListGroup.Item className="devices">
+                  Devices: {issue.Devices}
+                </ListGroup.Item>
+                <ListGroup.Item className="devices">
+                  State: {issue.state}
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>{" "}
+            <Container>
+              <Row>
+                <Col></Col>
+                <Col>
+                  <br></br>
+                  <Accordion>
+                    <Accordion.Item>
+                      <Accordion.Header>View Recommendation</Accordion.Header>
+                      <Accordion.Body>
+                        {
+                          <div>
+                            <h2>What happened in {issue["Issue Id"]}</h2>
+                            <p>{issue.Description.split("##")[0]}}</p>
+                            <p>{issue.Description.split("##")[1]}}</p>
+                            <p>{issue.Description.split("##")[2]}}</p>
+                            <p>{issue.Description.split("##")[3]}}</p>
+                            <pre>
+                              <code class="language-html">
+                                <p>{issue.Description.split("##")[4]}}</p>
+                              </code>
+                            </pre>
+                          </div>
+                        }
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </Col>
+                <Col></Col>
+              </Row>
+              <br></br>  <br></br>  <br></br>  <br></br>  <br></br>
+            </Container>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default LocalIssues;
