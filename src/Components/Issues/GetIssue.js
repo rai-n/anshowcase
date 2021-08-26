253a70f61b238e8131e6cff0f6de0b4694aa2ea9
rai-n <neeraj.rai@city.ac.uk>
2021-08-26 11:00:02 +0100

    final

--- src/Components/Issues/GetIssue.js
+++ src/Components/Issues/GetIssue.js
@@ -6,7 +6,6 @@ import Accordion from "react-bootstrap/Accordion";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
-import Posts from "./Post";
 import "../../issue.css";
 class LocalIssues extends Component {
   constructor(props) {
@@ -24,36 +23,11 @@ class LocalIssues extends Component {
       companies.add(user.Customer);
     });
 
-    
     let success = new Set();
     issuesJSON.forEach((user) => {
       success.add(user["Success Criteria"]);
     });
 
- 
- 
-    const postsPerPage = 3;
-    let arrayForHoldingPosts = [];
-
-    const App = () => {
-      const [postsToShow, setPostsToShow] = useState([]);
-      const [next, setNext] = useState(3);
-    
-      const loopWithSlice = (start, end) => {
-        const slicedPosts = posts.slice(start, end);
-        arrayForHoldingPosts = [...arrayForHoldingPosts, ...slicedPosts];
-        setPostsToShow(arrayForHoldingPosts);
-      };
-
-      useEffect(() => {
-        loopWithSlice(0, postsPerPage);
-      }, []);
-    
-      const handleShowMorePosts = () => {
-        loopWithSlice(next, next + postsPerPage);
-        setNext(next + postsPerPage);
-      };  
- 
     return (
       <div>
         <h1 className="center">6 month issues exported from YouTrack</h1>
@@ -88,8 +62,8 @@ class LocalIssues extends Component {
                 {
                   //converting set to array
                   [...success].map((name) => (
-                    // not using list style 
-                    <li align="start" style={{listStyleType: "none"}}>
+                    // not using list style
+                    <li align="start" style={{ listStyleType: "none" }}>
                       <div>{name}</div>
                     </li>
                   ))
@@ -100,11 +74,12 @@ class LocalIssues extends Component {
               {" "}
               <h2>Summary</h2>
               <p>
-                In this YouTrack export of last six months, I have written well over <b>{issues.length}</b> issues. Identifying {issues.length} that may impact accessibility and usability for users with vision, auditory, motor or cognitive impairments.
+                In this YouTrack export of last six months, I have written well
+                over <b>{issues.length}</b> issues. Identifying {issues.length}{" "}
+                that may impact accessibility and usability for users with
+                vision, auditory, motor or cognitive impairments.
               </p>
-              <p>
-                See the issues below for more detail.
-              </p>
+              <p>See the issues below for more detail.</p>
             </Col>
           </Row>
           <br></br>
@@ -112,12 +87,69 @@ class LocalIssues extends Component {
           <br></br>
           <br></br>
         </Container>
-      
+
         {issues.map((issue) => (
           <div>
-              <Post postsToRender={postsToShow}></Post>
-              <br></br>  <br></br>  <br></br>  <br></br>  <br></br>
-              <button onClick={handleShowMorePosts}>Load more</button>
+            <Card style={{ width: "18rem" }} className="container">
+              <ListGroup variant="flush">
+                <ListGroup.Item className="component">
+                  Summary: {issue.Summary}
+                </ListGroup.Item>
+                <ListGroup.Item className="component">
+                  Component: {issue.Component}
+                </ListGroup.Item>
+                <ListGroup.Item className="prior">
+                  Success Criteria: {issue["Success Criteria"]}
+                </ListGroup.Item>
+                <ListGroup.Item className="devices">
+                  Devices: {issue.Devices}
+                </ListGroup.Item>
+                <ListGroup.Item className="devices">
+                  State: {issue.state}
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>{" "}
+            <Container>
+              <Row>
+                <Col></Col>
+                <Col>
+                  <br></br>
+                  <Accordion>
+                    <Accordion.Item>
+                      <Accordion.Header>View Recommendation</Accordion.Header>
+                      <Accordion.Body>
+                        {
+                          <div>
+                            <h2>{issue.Summary}</h2>
+                            <p>{issue.Description.split("##")[0]}</p>
+                            <h3>What happened</h3>
+                            <p>{issue.Description.split("##")[1]}</p>
+                            <h3>Where it happened</h3>
+                            <p>{issue.Description.split("##")[2]}</p>
+                            <h3>Steps to reproduce</h3>
+                            {issue.Description.split("##")[3]
+                              .split(/[1-4]/g)
+                              .map((item) => (
+                                <p>
+                                  <div>{item}</div>
+                                </p>
+                              ))}
+
+                            <pre>
+                              <code class="language-html">
+                                <h3>Recommendation</h3>
+                                <p>{issue.Description.split("##")[4]}}</p>
+                              </code>
+                            </pre>
+                          </div>
+                        }
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+                </Col>
+                <Col></Col>
+              </Row>
+              <br></br> <br></br> <br></br> <br></br> <br></br>
             </Container>
           </div>
         ))}
